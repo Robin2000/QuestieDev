68c58e2b6eed95f0cbc78a935d9ecb771f8798e9
13011054008 <29698868@qq.com>
2018-12-07 13:51:16 +0800

    去除视图创建语句中的库名

--- SQLData/Views/item_objectdata.sql
+++ SQLData/Views/item_objectdata.sql
@@ -1,4 +1,4 @@
 CREATE VIEW `item_objectdata` AS
 SELECT ol.item AS 'item_id', ol.entry AS 'objectdata_id'
-FROM mangos.gameobject_loot_template ol
-WHERE ol.item IN (SELECT id FROM questie.items)
+FROM gameobject_loot_template ol
+WHERE ol.item IN (SELECT id FROM items)
--- SQLData/Views/item_vendors.sql
+++ SQLData/Views/item_vendors.sql
@@ -1,4 +1,4 @@
 CREATE VIEW `item_vendors` AS
 SELECT v.item AS 'item_id', v.entry AS 'monster_id'
-FROM mangos.npc_vendor v
-WHERE v.item IN (SELECT id FROM questie.items)
+FROM npc_vendor v
+WHERE v.item IN (SELECT id FROM items)
